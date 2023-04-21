c42d568febe7a90ef1e356aeaf88b401bfea6c67
Mohamed TRAORE <39139566+apollosoldier@users.noreply.github.com>
2023-04-21 13:00:22 +0200

    Update base_classifier.py

--- src/base_classifier.py
+++ src/base_classifier.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
+from torchvision.models import resnet34
 
 class BaseClassifier(nn.Module):
-    def __init__(self, num_classes= 10):
+    def __init__(self, num_classes=10):
         """
-        Initialize the BaseClassifier object by defining the layers of the neural network.
+        Initialize the BaseClassifier object by using a pre-trained ResNet34 model.
 
         Input:
         @param - num_classes: the number of classes to be classified
@@ -13,19 +14,8 @@ class BaseClassifier(nn.Module):
         @return - None
         """
         super(BaseClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-
-        self.fc1 = nn.Linear(128 * 8 * 8, 512)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.fc2 = nn.Linear(512, num_classes)
+        self.resnet = resnet34(pretrained=True)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
 
     def forward(self, x):
         """
@@ -37,18 +27,4 @@ class BaseClassifier(nn.Module):
         Output:
         @return - the output of the neural network
         """
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu3(x)
-        x = self.fc2(x)
-        return x
+        return self.resnet(x)
